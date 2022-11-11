e01443c83d531572190878b0d7027883d6e36782
quackercrumbs <calvinquach@gmail.com>
2022-11-11 17:48:45 -0500

    p2: fix read message to handle errors and add handle session fn
    TODO:
    - for read_message, check for Ok(0) and properly handling EOF.
        - we probably don't want to let UnexpectedEof dictate that we've reach
          the end of the stream.
    - update integration test to actually test app

--- p2/README.md
+++ p2/README.md
@@ -1,3 +1,12 @@
+```
+// Only run one thread, any test involving server have a race condition for the host:port used in the app.
+// Since test threads defaults to number of CPU cores, tests can run in parallel.
+// Ideally, we either spin up the server once, or we dynamically choose ports
+
+cargo test -j 1 -- --test-threads 1
+
+```
+
 https://protohackers.com/problem/2
 
 Your friendly neighbourhood investment bank is having trouble analysing historical price data. They need you to build a TCP server that will let clients insert and query timestamped prices.
--- p2/rust/src/main.rs
+++ p2/rust/src/main.rs
@@ -1,6 +1,8 @@
-use tokio::io::{AsyncRead, AsyncReadExt};
+use std::io;
+use std::net::SocketAddr;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tracing::dispatcher::DefaultGuard;
-use tracing::{debug, info, error};
+use tracing::{debug, error, info};
 use tracing_subscriber::prelude::*;
 
 fn main() {
@@ -52,33 +54,116 @@ fn handle_avg_query(storage: &Vec<PricePoint>, query: QueryRange) -> i32 {
     }
 }
 
-async fn read_message(stream: &mut (impl AsyncRead + std::marker::Unpin)) -> (char, i32, i32) {
+async fn read_message(
+    stream: &mut (impl AsyncRead + std::marker::Unpin),
+) -> io::Result<(char, i32, i32)> {
     // for the read stream, read the 9 bytes
-    let message_type = stream.read_u8().await.unwrap();
-    let field_1 = stream.read_i32().await.unwrap();
-    let field_2 = stream.read_i32().await.unwrap();
+    let message_type = stream.read_u8().await?;
+    let field_1 = stream.read_i32().await?;
+    let field_2 = stream.read_i32().await?;
 
-    (char::from(message_type), field_1, field_2)
+    // TODO: we probably want to check read len is 0 for EOF, instead of UnexpectedEof error.
+    //    It's kinda hard to judge thou, since the docs for read also say that len == 0 may not be
+    //    trust worthy...
+    //    ref: https://docs.rs/tokio/1.21.2/tokio/io/trait.AsyncReadExt.html#return
+    Ok((char::from(message_type), field_1, field_2))
 }
 
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::oneshot;
 
 async fn serve(ready_signal: oneshot::Sender<bool>) {
-    let listener = TcpListener::bind("0.0.0.0:8000").await.expect("Couldn't start tcp listener on addres");
+    let listener = TcpListener::bind("0.0.0.0:8000")
+        .await
+        .expect("Couldn't start tcp listener on addres");
     info!("Listening on address: {:?}", listener.local_addr());
-    ready_signal.send(true).expect("Couldn't send ready signal after server has started");
+    ready_signal
+        .send(true)
+        .expect("Couldn't send ready signal after server has started");
 
     loop {
         let stream = listener.accept().await;
         match stream {
-            Ok(stream) => {
-                info!("Accepted connection for {:?}", stream);
+            Ok((stream, socket_addr)) => {
+                info!("Accepted connection for {:?}", socket_addr);
+                tokio::spawn(async move { handle_session(stream, socket_addr).await });
             }
             Err(e) => {
                 error!("Error when listening for connection, {:?}", e);
             }
         };
+    }
+}
+
+async fn handle_session(mut stream: TcpStream, remote_addr: SocketAddr) {
+    let mut storage: Vec<PricePoint> = Vec::new();
+    let (mut read_s, mut write_s) = stream.split();
+    loop {
+        let message_result = read_message(&mut read_s).await;
+        match message_result {
+            Ok((r_type, field_1, field_2)) => {
+                match r_type {
+                    'I' => {
+                        // rename to something more meaniningful
+                        let timestamp = field_1;
+                        let price = field_2;
+                        handle_insert(&mut storage, PricePoint(timestamp, price));
+                    }
+                    'Q' => {
+                        // rename to something more meaningful
+                        let min_time = field_1;
+                        let max_time = field_2;
+                        let ret = handle_avg_query(
+                            &storage,
+                            QueryRange {
+                                start: min_time,
+                                end: max_time,
+                            },
+                        );
+                        write_s
+                            .write_i32(ret)
+                            .await
+                            .expect(format!("Error when processing {:?}", remote_addr).as_str());
+                    }
+                    invalid_type => {
+                        error!(
+                            "lmao yo get outta here with that fake type: {:?}",
+                            invalid_type
+                        );
+                        panic!("AHHHH");
+                    }
+                }
+            }
+            Err(e) => {
+                info!("Error reading for {:?} : {:?}", remote_addr, e);
+                break;
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod integration_tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_problem() {
+        let _guard = setup_tracing();
+        let (ready_sender, ready_receiver) = oneshot::channel();
+        let server_handle = tokio::spawn(async {
+            serve(ready_sender).await;
+        });
+        let _ready_signal = ready_receiver.await;
+
+        let client_handle = tokio::spawn(async {
+            // todo: send request to server
+        });
+        let client_result = client_handle.await;
+        debug!("client_result={:?}", client_result);
+        assert!(client_result.is_ok());
+
+        server_handle.abort();
+
     }
 }
 
@@ -91,14 +176,16 @@ mod server_tests {
     async fn test_server_startup() {
         let _guard = setup_tracing();
         let (ready_sender, ready_receiver) = oneshot::channel();
-        tokio::spawn(async {
+        let server_handle = tokio::spawn(async {
             serve(ready_sender).await;
         });
 
         let ready_signal = ready_receiver.await;
         assert_eq!(Ok(true), ready_signal);
-    }
 
+
+        server_handle.abort();
+    }
 }
 
 #[cfg(test)]
@@ -118,7 +205,16 @@ mod parsing_tests {
         ]);
         let result = read_message(&mut reader).await;
         info!("results = {:?}", result);
-        assert_eq!(('Q', 1, 2), result);
+        assert_eq!(('Q', 1, 2), result.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_parsing_empty() {
+        let _guard = setup_tracing();
+        let mut reader = Cursor::new(vec![]);
+        let result = read_message(&mut reader).await;
+        info!("results = {:?}", result);
+        assert_eq!(result.is_err(), true);
     }
 }
 
